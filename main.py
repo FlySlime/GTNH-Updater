62e70b49748607d4e295ae1ccc64c65616dd5a9f
FlySlime <ibrabdu@student.chalmers.se>
2023-02-20 22:54:03 +0100

    Move additional-mods into the new folder

--- main.py
+++ main.py
@@ -347,7 +347,7 @@ def update_client(path, file_name, shader_answer):
 
     # Move additional mods, if any, to the game folder
     print("Searching for additional mods..." + total_progress())
-    additional_mods_dir = "./additional-mods-client"
+    additional_mods_dir = updater_files_dir + "additional-mods-client"
     copy_dir_to_game(additional_mods_dir, path)
 
     # Move shaders folder, if user choose so
@@ -422,7 +422,7 @@ def update_server(path, file_name):
 
     # Move additional mods, if any, to the server folder
     print("Searching for additional mods..." + total_progress())
-    additional_mods_dir = "./additional-mods-server"
+    additional_mods_dir = updater_files_dir + "additional-mods-server"
     copy_dir_to_game(additional_mods_dir, path)
 
     # Move into the server directory
